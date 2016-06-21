8998815ebe221ec241c6f039a801acd32ea07e43
Ankesh Anand <ankeshanand1994@gmail.com>
2016-06-22 00:27:24 +0530

    Add script to download data files

---
+++ models/LSTM/data/download_data.sh
@@ -0,0 +1,8 @@
+
+# Download word vectors
+wget -c https://googledrive.com/host/0BwwuaCBVtxIDaXNjbDExSWVOeWM/GoogleNews-vectors-negative300.bin.gz
+
+# Download dataset files
+wget -c http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books.json.gz
+wget -c http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Electronics.json.gz
+wget -c http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Home_and_Kitchen.json.gz
